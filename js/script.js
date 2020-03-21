88146db908412d0dff0c7543343db5184c69d6a0
zhoulei17 <tcals1@163.com>
2020-03-21 23:17:42 +0800

    Site updated: 2020-03-21 23:17:41

--- js/script.js
+++ js/script.js
@@ -416,7 +416,7 @@ $(function () {
     }
     if ($(window).width() > 414) {
         /*设置文章列表title宽度*/
-        $('.nav-right>nav>a>.post-title').css('width',$('.nav-right>nav>a').width() - $('.nav-right>nav>a>.post-date:first').width() - 40)
+        $('.nav-right>nav>a>.post-title').css('width',$('.nav-right>nav>a').width())
     }
     // 初始化tag列表宽度
     $('.tags-list').css('width', $('.nav-right').width() - 40)
